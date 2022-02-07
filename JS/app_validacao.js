4641418b3d192ab886e388ef6506b147ce057e4d
Guilherme Santiago de Oliveira <guisanti.oliveira@gmail.com>
2022-02-07 20:36:53 -0300

    Delete app_validacao.js
    Deletando conteúdo repetido

--- JS/app_validacao.js
+++
@@ -1,9 +0,0 @@
-import {valida} from './script'
-
-const inputs = document.querySelectorAll('input');
-
-inputs.forEach(input => {
-    input.addEventListener('blur', (evento) => {
-        valida(evento.target);
-    });
-});
