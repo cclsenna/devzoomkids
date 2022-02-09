24f3111b1e4b36b08dd6198ff1f9ffda3124fe7b
Caio Senna <caiosenna93@gmail.com>
2022-02-09 13:02:13 -0300

    refazendo form de redefinição de senha

--- assets/scripts/comportamentos.js
+++ assets/scripts/comportamentos.js
@@ -5,6 +5,7 @@ document.querySelector('#botao-modal-recuperar').addEventListener('click',()=>{
 });
 
 
+
 //redireciona
 document.querySelector('#botao-modal-redefinir').addEventListener('click',()=>{
     console.log('entrou em redefinir');
