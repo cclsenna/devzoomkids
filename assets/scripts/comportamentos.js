e60a936f8b811cf59ee20b6f72c23c74bffca537
Caio Senna <caiosenna93@gmail.com>
2022-02-09 17:18:50 -0300

    correção de comportamento de troca de senha

--- assets/scripts/comportamentos.js
+++ assets/scripts/comportamentos.js
@@ -1,3 +1,5 @@
+/*
+
 //redireciona
 document.querySelector('#botao-modal-recuperar').addEventListener('click',()=>{
     console.log('entrou em recuperar');
@@ -10,5 +12,22 @@ document.querySelector('#botao-modal-recuperar').addEventListener('click',()=>{
 document.querySelector('#botao-modal-redefinir').addEventListener('click',()=>{
     console.log('entrou em redefinir');
     window.location.href = "./index.html";
+});
+
+*/
+
+//Adicionando evneto para capturar qualquer click  na tela
+document.addEventListener('click',(e)=>{
+    //verifica se o botão clickado foi o do modal 1
+    if(e.target.id==='botao-modal-recuperar'){
+        window.location.href = "./redefinir_Senha.html";
+
+    }
+    //verifica se o botão clickado foi o do modal 2
+
+    else if(e.target.id==='botao-modal-redefinir'){
+        window.location.href = "./index.html";
+    }
+
 });
 
