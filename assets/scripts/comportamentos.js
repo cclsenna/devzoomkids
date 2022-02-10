5b7fda53249965ac3f236ba705c774c2ff94880a
Caio Senna <caiosenna93@gmail.com>
2022-02-10 19:35:54 -0300

    tela codigo de recuperçaão de senha

--- assets/scripts/comportamentos.js
+++ assets/scripts/comportamentos.js
@@ -1,47 +1,48 @@
-/*
-
-//redireciona
-document.querySelector('#botao-modal-recuperar').addEventListener('click',()=>{
-    console.log('entrou em recuperar');
-    window.location.href = "./redefinir_Senha.html";
-});
-
-
-
-//redireciona
-document.querySelector('#botao-modal-redefinir').addEventListener('click',()=>{
-    console.log('entrou em redefinir');
-    window.location.href = "./index.html";
-});
-
-*/
 
 //Adicionando evneto para capturar qualquer click  na tela
+
+
 document.addEventListener('click',(e)=>{
-    //verifica se o botão clickado foi o do formulario 1
-    let formulario=document.querySelector('.form-pass');
-    if(e.target.id==='botao-recuperar'&&formulario.checkValidity()){
+    //let formulario=document.querySelector('.form-pass');
+
+    //verifica se o botão clickado foi o do tela de email   
+
+    if(e.target.id==='botao-recuperar'){
         e.preventDefault();
-        let a=document.getElementById('emailHelp');
-        let b=document.getElementById('emailSuccess');
-        a.style.display='none';
-        b.style.display='block';
-        document.getElementById('inputEmail4').value='';
-        return;
-        
+        alternaTela();
+    }  
 
-    }
-    //verifica se o botão clickado foi o do mformulario 2
-
-    else if(e.target.id==='botao-redefinir'){
-        e.preventDefault();
-        window.location.href = "./index.html";
-    }
-
-    else if(e.target.id==='botao-recuperar'){
-        e.preventDefault();
-        console.log(e.target);
-    }
-
+    
 });
+
+
+
+
+function alternaTela(){
+    let contEmail=document.getElementById('cont');
+    let contCode=document.getElementById('cont2');
+    let emailCLiente=document.getElementById('inputEmail4').value;
+    contEmail.classList.remove('d-flex');
+    contEmail.classList.add('d-none');
+    contCode.classList.remove('d-none');
+    contCode.classList.add('d-flex');  
+    document.getElementById('inputEmail4').value='';
+    console.log(emailCLiente);
+    
+    return;
+
+}
+
+//funcao que omite alguns caracteres do email do cliente
+function criptoEmail(value){
+    let pos=value.indexOf('@');
+    let newValue=value.split('');
+
+    for(let i=1;i<=pos;i++){
+        newValue[i]='*'
+    }
+    return newValue.join('');
+
+
+}
 
