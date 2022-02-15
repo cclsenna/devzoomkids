038653447ee42727675dea1e0034672668b52bcd
Caio Senna <caiosenna93@gmail.com>
2022-02-14 21:54:51 -0300

    correção deploy v1

--- assets/scripts/app_validacao.js
+++
@@ -1,9 +0,0 @@
-import {valida} from './script.js'
-
-const inputs = document.querySelectorAll('input');
-
-inputs.forEach(input => {
-    input.addEventListener('blur', (evento) => {
-        valida(evento.target);
-    });
-});
